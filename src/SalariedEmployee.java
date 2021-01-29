a6c5e137d50690026ffad9f3a2fcedb83387cc58
anastasiia-titarenko1 <titnastya92@gmail.com>
2021-01-29 12:08:48 +0200

    hw 04 done

--- src/SalariedEmployee.java
+++ src/SalariedEmployee.java
@@ -1,27 +1,38 @@
-public class SalariedEmployee {
+public class SalariedEmployee extends Employee {
   // TODO fix class declaration and declare variables here
+  double fixedMonthlyPayment;
+  String socialSecurityNumber;
 
   public SalariedEmployee(String employeeId, String name, String socialSecurityNumber) {
     // TODO fill in code here
+    super(employeeId, name);
+    this.socialSecurityNumber = socialSecurityNumber;
   }
 
   public String getSocialSecurityNumber() {
     // TODO fill in code here and replace the return statement
-    return "";
+    return this.socialSecurityNumber;
   }
 
   public void setSocialSecurityNumber(String socialSecurityNumber) {
     // TODO fill in code here
+    this.socialSecurityNumber = socialSecurityNumber;
   }
 
   public double getFixedMonthlyPayment() {
     // TODO fill in code here and replace the return statement
-    return 0;
+    return this.fixedMonthlyPayment;
   }
 
   public void setFixedMonthlyPayment(double fixedMonthlyPayment) {
     // TODO fill in code here
+    this.fixedMonthlyPayment = fixedMonthlyPayment;
   }
 
-  // TODO fill in code here
+  public double getAverageMonthlySalary() {
+    // TODO fill in code here and replace the return statement
+    return fixedMonthlyPayment;
+  }
+
+
 }
